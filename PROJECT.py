704884d251484b39af3b223360205ceca68c30ac
YOGESH KUMAR <84972705+yogeshchoudhary209@users.noreply.github.com>
2024-08-10 17:58:26 +0530

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.optimizers import RMSprop
@@ -13,59 +7,23 @@ import numpy as np
 import cv2 as cv
 import os
 
-
-# In[3]:
-
-
 img = image.load_img("C:\DEEP LEARNING- PROJECT\VALIDATION\HAPPY\PP.JPG")
-
-
-# In[4]:
-
 
 plt.imshow(img)
 
-
-# In[6]:
-
-
 cv.imread("C:\DEEP LEARNING- PROJECT\VALIDATION\HAPPY\PP.JPG")
-
-
-# In[7]:
-
 
 train = ImageDataGenerator(rescale= 1/255)
 validation =ImageDataGenerator(rescale= 1/255)
 
-
-# In[8]:
-
-
 train_dataset =train.flow_from_directory('C:\DEEP LEARNING- PROJECT\TRAINING FOLDER', target_size= (200, 200), batch_size= 3, class_mode= 'binary')
 validation_dataset= validation.flow_from_directory('C:\DEEP LEARNING- PROJECT\VALIDATION', target_size= (200, 200), batch_size= 3, class_mode= 'binary')
 
-
-# In[9]:
-
-
 model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16,(3, 3), activation= 'relu', input_shape= (200, 200,3)), tf.keras.layers.MaxPool2D(2,2), tf.keras.layers.Conv2D(32,(3,3), activation= 'relu'), tf.keras.layers.MaxPool2D(2,2), tf.keras.layers.Conv2D(64, (3,3), activation='relu'), tf.keras.layers.MaxPool2D(2, 2), tf.keras.layers.Flatten(), tf.keras.layers.Dense(512, activation='relu'), tf.keras.layers.Dense(1, activation='sigmoid')])
-
-
-# In[10]:
-
 
 model.compile(loss= 'binary_crossentropy', optimizer= RMSprop(lr=0.001), metrics= ['accuracy'])
 
-
-# In[24]:
-
-
 model_fit = model.fit(train_dataset, steps_per_epoch = 5, epochs = 20, validation_data= validation_dataset)
-
-
-# In[25]:
-
 
 dir_path = 'C:\DEEP LEARNING- PROJECT\TESTING'
 for  i in os.listdir(dir_path):
@@ -81,10 +39,3 @@ for  i in os.listdir(dir_path):
         print("you are not happy")
     else:
         print("you are happy")
-
-
-# In[ ]:
-
-
-
-
